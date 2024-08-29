1c5d70c8659c61ec6a5c2325d07e60eb2d33eaf7
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2024-08-29 15:45:41 +0530

    setup

---
+++ src/Box.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract Box is Ownable{
+
+    uint256 private s_number;
+
+    constructor() Ownable(msg.sender){}
+
+    event NumberChanged(uint256 number);
+
+    function getNumber(uint256 newNumber) public {
+        s_number = newNumber;
+        emit NumberChanged(newNumber);
+    }
+
+    function getNumber() external view returns(uint256){
+        return s_number;
+    }
+}
